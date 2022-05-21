446857e588e9a392ece0d10acdf00414efb6e08d
Antony Quang <antony.quang@googlemail.com>
2022-05-21 21:35:59 +0100

    login authentication added

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,12 +27,12 @@ class User(UserMixin, db.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(user_id)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -44,11 +45,15 @@ def register():
             password=generate_password_hash(entry["password"], method='pbkdf2:sha256', salt_length=8),
             name=entry["name"]
         )
-        db.session.add(new_entry)
-        db.session.commit()
-        print(new_entry.name)
-        return redirect(url_for('secrets', name=new_entry.name))
-    return render_template("register.html")
+
+        if db.session.query(User).filter(User.email == new_entry.email).first():
+            flash("You've already signed up with that email, log in instead")
+        else:
+            db.session.add(new_entry)
+            db.session.commit()
+
+            return redirect(url_for('secrets', name=new_entry.name))
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -56,30 +61,40 @@ def login():
     if request.method == "POST":
         form = request.form
 
-        user = User(
-            email=form["email"],
-            password=form["password"],
-        )
-        user_in_database = db.session.query(User).filter(User.email == user.email).first()
+        user = {
+            "email": form["email"],
+            "password": form["password"],
+        }
+        user_in_database = db.session.query(User).filter(User.email == user["email"]).first()
+        if user_in_database:
 
-        if check_password_hash(user_in_database.password, user.password):
-            login_user(user)
-            flash("logged in successfully")
-            return "logged in"
-    return render_template("login.html")
+            if check_password_hash(user_in_database.password, user["password"]):
+
+                login_user(user_in_database)
+                return redirect(url_for('secrets'))
+            else:
+                flash("Passwords do not match")
+        else:
+            flash("Email not found")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets', methods=["GET"])
+@login_required
 def secrets():
-    return render_template("secrets.html", name=request.args.get('name'))
+    if current_user.is_authenticated:
+        logged_in = True
+    return render_template("secrets.html", name=current_user.name, logged_in=logged_in)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return render_template("index.html")
 
 
 @app.route('/download', methods=["GET"])
+@login_required
 def download():
     try:
         return send_from_directory(app.config['FILES'], path="cheat_sheet.pdf")
